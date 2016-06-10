4665b37a4faa1b7ff2ab6309d673c58d0bc16fd4
Psihokot <aterentiev89@gmail.com>
2016-06-10 12:38:05 +0300

    fixed busket.html
    fixed busket.html

--- js/js-busket.js
+++ js/js-busket.js
@@ -1,37 +1,75 @@
 $(function() {
+    var countPrice = function(price, target, counter) {
+        var costOf = parseInt(price) * counter;
+        var changedElement = $(target).parent().nextAll(".table_col_cost").children("input");
+        
+        $(changedElement).attr("value", costOf);
+    };
+    
+    var countTotalNumberof = function() {
+        var arr = $(".main_content_table > tbody").find(".table_col_numberof");
+        var totalCounter = 0;
+        // console.log(arr);
+
+        for (var i = 0; i < arr.length; i++) {
+            totalCounter = totalCounter + parseInt($($(arr)[i]).children("input").attr("value"));
+        }
+        $(".total_numberof").attr("value", totalCounter);
+    };
+
+    var countTotalPrice = function() {
+        var arr = $(".main_content_table > tbody").find(".table_col_cost");
+        var totalPrice = 0;
+        console.log(arr);
+
+        for (var i = 0; i < arr.length; i++) {
+            totalPrice = totalPrice + parseInt($($(arr)[i]).children("input").attr("value"));
+        }
+        $(".total_price").attr("value", totalPrice);
+    };
+
+    countTotalNumberof();
+    countTotalPrice();
+
     $(".del_row_icon").on("click", function(event) {
         var target = event.target;
+        var totalCounterMinus = $(target).parent().prevAll(".table_col_numberof").children("input").attr("value");
 
         $(target).parent().parent().remove();
+        countTotalNumberof();
+        countTotalPrice();
     });
-    
-    var totalCounter = $(".total_numberof").attr("value");
 
     $(".table_numberof_minus").on("click", function(event) {
         var target = event.target;
         var counter = $(target).next().attr("value");
+        var price = $(target).parent().prevAll(".table_col_price").html();
 
         if (counter == 0) {
             return
         } else {
             --counter;
-            --totalCounter;
         }
+
         $(target).next().attr("value", counter);
-        $(".total_numberof").attr("value", totalCounter);
+        countPrice(price, target, counter);
+        countTotalNumberof();
+        countTotalPrice();
     });
 
     $(".table_numberof_plus").on("click", function(event) {
         var target = event.target;
         var counter = $(target).prev().attr("value");
+        var price = $(target).parent().prevAll(".table_col_price").html();
         
         ++counter;
-        ++totalCounter;
         $(target).prev().attr("value", counter);
-        $(".total_numberof").attr("value", totalCounter);
+        countPrice(price, target, counter);
+        countTotalNumberof();
+        countTotalPrice();
     });
 
-
-
+    
+    
 });
 
